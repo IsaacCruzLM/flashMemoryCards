625d7888631d3916d54976cc303c28a99da74d11
Isaac Cruz <isaac.cruz@kartado.com.br>
2022-12-04 11:55:57 -0300

    Add animation in Initial Page

--- src/screens/InitialPage/index.tsx
+++ src/screens/InitialPage/index.tsx
@@ -1,5 +1,13 @@
-import * as React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {View} from 'react-native';
+import Animated, {
+  withTiming,
+  useAnimatedStyle,
+  interpolate,
+  Extrapolate,
+  useSharedValue,
+  withSequence,
+} from 'react-native-reanimated';
 
 import NavigationService from '../../navigation/NavigationService';
 
@@ -11,24 +19,82 @@ import Button from '../../components/Button';
 import styles from './styles';
 
 const InitialPage = () => {
+  const logoPosition = useSharedValue(0);
+  const titlePosition = useSharedValue(30);
+  const subTitlePosition = useSharedValue(30);
+  const buttonOpacity = useSharedValue(0);
+
+  useEffect(() => {
+    titlePosition.value = withTiming(0, {duration: 500}, () => {
+      subTitlePosition.value = withTiming(0, {duration: 500}, () => {
+        buttonOpacity.value = withTiming(1, {duration: 500});
+      });
+    });
+  });
+
+  useEffect(() => {
+    logoPosition.value = withSequence(
+      withTiming(10, {duration: 750}),
+      withTiming(0, {duration: 750}),
+    );
+  });
+
+  const titleStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{translateY: titlePosition.value}],
+      opacity: interpolate(
+        titlePosition.value,
+        [30, 0],
+        [0, 1],
+        Extrapolate.CLAMP,
+      ),
+    };
+  });
+
+  const subTitleStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{translateY: subTitlePosition.value}],
+      opacity: interpolate(
+        subTitlePosition.value,
+        [30, 0],
+        [0, 1],
+        Extrapolate.CLAMP,
+      ),
+    };
+  });
+
+  const buttonStyle = useAnimatedStyle(() => {
+    return {
+      opacity: buttonOpacity.value,
+    };
+  });
+
+  const logoViewStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{translateY: logoPosition.value}],
+    };
+  });
+
   return (
     <LinearGradientView>
       <View style={styles.container}>
-        <View>
+        <Animated.View style={logoViewStyle}>
           <LogoSvg height={78} width={93} fill="#ffffff" />
-        </View>
+        </Animated.View>
         <View style={styles.buttonContainer}>
-          <Text style={styles.title}>
+          <Animated.Text style={[styles.title, titleStyle]}>
             {`Bem vindo(a) ao${'\n'}Flash Memory${'\n'}Cards`}
-          </Text>
-          <Text style={styles.subTitle}>
+          </Animated.Text>
+          <Animated.Text style={[styles.subTitle, subTitleStyle]}>
             Seja relembrado de suas anotações, para que o conhecimento seja
             realmente fixado!
-          </Text>
-          <Button
-            onPress={() => NavigationService.navigate('Tutorial')}
-            label="Entrar"
-          />
+          </Animated.Text>
+          <Animated.View style={buttonStyle}>
+            <Button
+              onPress={() => NavigationService.navigate('Tutorial')}
+              label="Entrar"
+            />
+          </Animated.View>
         </View>
       </View>
     </LinearGradientView>
