114cf1565d9a482197dabd0284e1bfbb177cdc73
Isaac Cruz <isaac.cruz@kartado.com.br>
2023-02-13 19:50:44 -0300

    Init the IconPreview component

---
+++ src/components/IconPreview/styles.ts
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+import themes from '../../styles/themes';
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    marginBottom: themes.spacing.unit * 6,
+  },
+});
+
+export default styles;
