29e1455febb667b7b51de38f198c325634faa49f
Isaac Cruz <isaac.cruz@kartado.com.br>
2023-03-20 20:08:27 -0300

    ADD a try/catch to the function insertItenInWMDB

--- src/databases/utils/insertItenInWMDB.ts
+++ src/databases/utils/insertItenInWMDB.ts
@@ -3,17 +3,21 @@ import {snakeCase} from 'snake-case';
 import {database} from '..';
 
 export default async function insertItenInWMDB(model: string, data: Object) {
-  const modelInSnakeCase = snakeCase(model);
-  const newItem = await database.write(async () => {
-    const itemCreated = await database
-      .get(modelInSnakeCase)
-      .create(wmdbModel => {
-        const dataKeys = Object.keys(data);
-        dataKeys.map((key: string) => {
-          (wmdbModel as Model | any)[key] = (data as Object | any)[key];
+  try {
+    const modelInSnakeCase = snakeCase(model);
+    const newItem = await database.write(async () => {
+      const itemCreated = await database
+        .get(modelInSnakeCase)
+        .create(wmdbModel => {
+          const dataKeys = Object.keys(data);
+          dataKeys.map((key: string) => {
+            (wmdbModel as Model | any)[key] = (data as Object | any)[key];
+          });
         });
-      });
-    return itemCreated;
-  });
-  return newItem;
+      return itemCreated;
+    });
+    return newItem;
+  } catch (error: any) {
+    return {error: true, message: error.message};
+  }
 }
