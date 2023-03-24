f588490ae1a2a60eaadc0519bff8c0739e9de0f6
Isaac Cruz <isaac.cruz@kartado.com.br>
2023-03-24 15:25:10 -0300

    ADD note list card in Note list screen

--- src/screens/Note/List/index.tsx
+++ src/screens/Note/List/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import {Text} from 'react-native';
 
 import DefaultContainerView from '../../../components/DefaultContainerView';
+import NoteListCard from '../../../components/NoteListCard';
 
 // import styles from './styles';
 
 const List = () => {
   return (
     <DefaultContainerView>
-      <Text>Página de Lista de Notas</Text>
+      <NoteListCard
+        title={'Titulo da anotação 2'}
+        creationDate={'24/04/1997'}
+        lastRevisionDate={'24/04/1998'}
+        noteType={'Texto'}
+        category={'Categoria X'}
+        subjects={[
+          {content: 'Assunto 1', color: '#C31717'},
+          {content: 'Assunto 2', color: '#8db4e0'},
+        ]}
+      />
     </DefaultContainerView>
   );
 };
