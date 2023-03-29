07e91577078b0ba569863cd0e27ede0178c130ab
Isaac Cruz <isaac.cruz@kartado.com.br>
2023-03-29 20:26:45 -0300

    Refactor the ModuleModel
    In the ModuleModel in WMDB, add the fields, icon and color, and add the relation has_many, for te relation 1:N with NoteModel

--- src/databases/models/moduleModel.ts
+++ src/databases/models/moduleModel.ts
@@ -3,7 +3,16 @@ import {field} from '@nozbe/watermelondb/decorators';
 
 export class ModuleModel extends Model {
   static table = 'modules';
+  static associations = {
+    notes: {type: 'has_many', foreignKey: 'module_id'},
+  } as const;
 
   @field('name')
   name!: string;
+
+  @field('icon')
+  icon!: string;
+
+  @field('color')
+  color!: string;
 }
