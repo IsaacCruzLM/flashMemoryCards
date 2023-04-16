a6363ddef2f8e98837df1fd45fbb508f915cc7b2
Isaac Cruz <isaac.cruz@kartado.com.br>
2023-04-16 15:03:00 -0300

    ADD categories query in NewNote screen

--- src/screens/Note/Create/index.tsx
+++ src/screens/Note/Create/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import {View} from 'react-native';
+import withObservables from '@nozbe/with-observables';
+import {withDatabase} from '@nozbe/watermelondb/DatabaseProvider';
+import {compose} from 'recompose';
 
 import DefaultContainerView from '../../../components/DefaultContainerView';
 import Form from '../../../components/Form';
@@ -8,13 +11,23 @@ import Select from '../../../components/Select';
 import styles from './styles';
 import {CreateFormProps} from './types';
 
-const Create: React.FunctionComponent<any> = () => {
+const Create: React.FunctionComponent<any> = ({categories}) => {
+  const translateOptions = (optionsArray: any) =>
+    optionsArray.map(({id, name, icon, color}: any) => ({
+      label: name,
+      value: id,
+      iconName: icon,
+      iconColor: color,
+    }));
   return (
     <DefaultContainerView>
       <Form
-        form={({}: CreateFormProps) => (
+        form={({setFieldValue}: CreateFormProps) => (
           <View style={styles.formContainer}>
-            <Select />
+            <Select
+              options={translateOptions(categories)}
+              onChange={value => setFieldValue('category', value)}
+            />
           </View>
         )}
         initialValues={{category: '', subjects: []}}
@@ -24,4 +37,11 @@ const Create: React.FunctionComponent<any> = () => {
   );
 };
 
-export default Create;
+export default compose(
+  withDatabase,
+  withObservables([], ({database}: any) => {
+    return {
+      categories: database.get('categories').query(),
+    };
+  }),
+)(Create);
