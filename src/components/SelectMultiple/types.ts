0b997ce3d2b4153dc175d0a0bc9ad2e00da70460
Isaac Cruz <isaac.cruz@kartado.com.br>
2023-04-19 20:24:45 -0300

    Init the SelectMultiple component

---
+++ src/components/SelectMultiple/types.ts
@@ -0,0 +1,13 @@
+export interface SelectProps {
+  options: [optionType];
+  onChange: (arg: string) => void;
+  defaultValue?: string[];
+  modalTitle: string;
+}
+
+export interface optionType {
+  label: string;
+  value: string;
+  iconName?: string;
+  iconColor?: string;
+}
