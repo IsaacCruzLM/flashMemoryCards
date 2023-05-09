6c88d7131eda39b8105831d74d5f737cecfafda1
Isaac Cruz <isaac.cruz@kartado.com.br>
2023-05-08 21:47:51 -0300

    Split the styles of the TextEditor component in a different file

--- src/components/TextEditor/index.tsx
+++ src/components/TextEditor/index.tsx
@@ -1,8 +1,10 @@
 import React, {LegacyRef} from 'react';
 import {useRef, useState} from 'react';
-import {StyleSheet, View, Dimensions} from 'react-native';
+import {View, Dimensions} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 import {actions, RichEditor, RichToolbar} from 'react-native-pell-rich-editor';
+
+import styles from './styles';
 
 const TextEditor = () => {
   const richText = useRef() as LegacyRef<RichEditor> | undefined;
@@ -53,64 +55,3 @@ const TextEditor = () => {
 };
 
 export default TextEditor;
-
-const styles = StyleSheet.create({
-  container: {
-    height: '90%',
-  },
-  richTextContainer: {
-    width: '100%',
-    maxHeight: Dimensions.get('screen').height - 50,
-  },
-  richTextEditorStyle: {
-    borderBottomLeftRadius: 10,
-    borderBottomRightRadius: 10,
-    borderWidth: 1,
-    borderColor: '#ccaf9b',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.23,
-    shadowRadius: 2.62,
-    elevation: 4,
-    fontSize: 20,
-  },
-  richTextToolbarStyle: {
-    backgroundColor: '#c6c3b3',
-    borderColor: '#c6c3b3',
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    borderWidth: 1,
-  },
-  errorTextStyle: {
-    color: '#FF0000',
-    marginBottom: 10,
-  },
-  saveButtonStyle: {
-    backgroundColor: '#c6c3b3',
-    borderWidth: 1,
-    borderColor: '#c6c3b3',
-    borderRadius: 10,
-    padding: 10,
-    width: '25%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.23,
-    shadowRadius: 2.62,
-    elevation: 4,
-    fontSize: 20,
-  },
-
-  textButtonStyle: {
-    fontSize: 18,
-    fontWeight: '600',
-    color: '#312921',
-  },
-});
---
+++ src/components/TextEditor/styles.ts
@@ -0,0 +1,65 @@
+import {StyleSheet, Dimensions} from 'react-native';
+// import themes from '../../styles/themes';
+
+const styles = StyleSheet.create({
+  container: {
+    height: '90%',
+  },
+  richTextContainer: {
+    width: '100%',
+    maxHeight: Dimensions.get('screen').height - 50,
+  },
+  richTextEditorStyle: {
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    borderWidth: 1,
+    borderColor: '#ccaf9b',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+    fontSize: 20,
+  },
+  richTextToolbarStyle: {
+    backgroundColor: '#c6c3b3',
+    borderColor: '#c6c3b3',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderWidth: 1,
+  },
+  errorTextStyle: {
+    color: '#FF0000',
+    marginBottom: 10,
+  },
+  saveButtonStyle: {
+    backgroundColor: '#c6c3b3',
+    borderWidth: 1,
+    borderColor: '#c6c3b3',
+    borderRadius: 10,
+    padding: 10,
+    width: '25%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+    fontSize: 20,
+  },
+
+  textButtonStyle: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#312921',
+  },
+});
+
+export default styles;
