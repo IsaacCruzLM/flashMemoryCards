7cf6a3e90569e5005237829ba0721e5aab5d266a
Isaac Cruz <isaac.cruz@kartado.com.br>
2023-05-13 15:26:15 -0300

    ADD init of the function insertItemWitmM2MRelationInWMDB

---
+++ src/databases/utils/insertItemWitmM2MRelationInWMDB.ts
@@ -0,0 +1,22 @@
+import get from 'lodash/get';
+
+import insertItemInWMDB from './insertItemInWMDB';
+
+export default async function insertItemWitmM2MRelationInWMDB(
+  model: string,
+  data: Object,
+  relationshipModel: string,
+  relationships: Array<object>,
+  relationshipeKey: string,
+) {
+  const itemCreated = await insertItemInWMDB(model, data);
+
+  relationships.map(async relationshipData => {
+    await insertItemInWMDB(relationshipModel, {
+      [relationshipeKey]: get(itemCreated, 'id', ''),
+      ...relationshipData,
+    });
+  });
+
+  return itemCreated;
+}
