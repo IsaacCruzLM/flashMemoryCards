01065d170e6c72b4697a449d6dca08d208e01faa
Isaac Cruz <isaac.cruz@kartado.com.br>
2023-05-21 12:44:46 -0300

    Edit styles of ShowNote screen

--- src/screens/Note/Show/index.tsx
+++ src/screens/Note/Show/index.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
+import {View} from 'react-native';
 
 import DefaultContainerView from '../../../components/DefaultContainerView';
 import TextEditor from '../../../components/TextEditor';
 
+import styles from './styles';
 import {ShowProps} from './types';
 
 const Show: React.FunctionComponent<ShowProps | any> = ({content}) => {
   return (
     <DefaultContainerView>
-      <TextEditor
-        initialContentHTML={content}
-        onChange={() => {}}
-        placeHolder="Escreva sua anotação aqui!"
-      />
+      <View style={styles.container}>
+        <TextEditor
+          initialContentHTML={content}
+          onChange={() => {}}
+          placeHolder="Escreva sua anotação aqui!"
+        />
+      </View>
     </DefaultContainerView>
   );
 };
---
+++ src/screens/Note/Show/styles.ts
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+import themes from '../../../styles/themes';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    // padding: themes.spacing.unit,
+  },
+});
+
+export default styles;
