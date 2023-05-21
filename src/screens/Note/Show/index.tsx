f304435279ccfc4ecc0e09a4ce0bfc6c0fc80a1e
Isaac Cruz <isaac.cruz@kartado.com.br>
2023-05-21 17:06:08 -0300

    ADD a view around text editor

--- src/screens/Note/Show/index.tsx
+++ src/screens/Note/Show/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {View} from 'react-native';
 import withObservables from '@nozbe/with-observables';
 import {withDatabase} from '@nozbe/watermelondb/DatabaseProvider';
@@ -7,12 +7,16 @@ import get from 'lodash/get';
 
 import DefaultContainerView from '../../../components/DefaultContainerView';
 import TextEditor from '../../../components/TextEditor';
+import Button from '../../../components/Button';
+
+import AppContext from '../../../context/appContext';
 
 import styles from './styles';
 import {ShowProps} from './types';
 
 const Show: React.FunctionComponent<ShowProps | any> = ({note}) => {
-  console.log(note.content);
+  const {globalState} = useContext(AppContext);
+
   return (
     <DefaultContainerView>
       <View style={styles.container}>
@@ -21,6 +25,11 @@ const Show: React.FunctionComponent<ShowProps | any> = ({note}) => {
           onChange={() => {}}
           placeHolder="Escreva sua anotação aqui!"
         />
+        {globalState.keyboardIsVisible ? null : (
+          <View>
+            <Button label={'Salvar Anotação'} onPress={() => {}} />
+          </View>
+        )}
       </View>
     </DefaultContainerView>
   );
