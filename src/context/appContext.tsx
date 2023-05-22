4339fc9603981e5c799fbe5ae4a6e8e6ae30624b
Isaac Cruz <isaac.cruz@kartado.com.br>
2023-05-22 19:22:40 -0300

    ADD the key showDialogEditNote to global state context

--- src/context/appContext.tsx
+++ src/context/appContext.tsx
@@ -2,18 +2,22 @@ import {createContext} from 'react';
 
 interface globalStateType {
   keyboardIsVisible: boolean;
+  showDialogEditNote: boolean;
 }
 
 interface ContextType {
   globalState: globalStateType;
   setKeyboardIsVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  setShowDialogEditNote: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const AppContext = createContext<ContextType>({
   globalState: {
     keyboardIsVisible: false,
+    showDialogEditNote: false,
   },
   setKeyboardIsVisible: () => {},
+  setShowDialogEditNote: () => {},
 });
 
 export default AppContext;
--- src/context/appProvider.tsx
+++ src/context/appProvider.tsx
@@ -7,14 +7,17 @@ export interface ProviderProps {
 
 const ContextProvider = ({children}: ProviderProps | any) => {
   const [keyboardIsVisible, setKeyboardIsVisible] = useState(false);
+  const [showDialogEditNote, setShowDialogEditNote] = useState(false);
 
   const globalState = {
     keyboardIsVisible,
+    showDialogEditNote,
   };
 
   const contextValue = {
     globalState,
     setKeyboardIsVisible,
+    setShowDialogEditNote,
   };
 
   return (
