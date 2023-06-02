2e8268f59a45d499dd713e4d272d9657a36ca1f1
Isaac Cruz <isaac.cruz@kartado.com.br>
2023-06-02 15:51:47 -0300

    ADD a utils function to show notifications toast

---
+++ src/utils/toastShow/index.ts
@@ -0,0 +1,23 @@
+import {ALERT_TYPE, Toast} from 'react-native-alert-notification';
+
+const toastShow = (type: string, title: string, textBody: string) => {
+  const getType = () => {
+    switch (type) {
+      case 'danger':
+        return ALERT_TYPE.DANGER;
+      case 'success':
+        return ALERT_TYPE.SUCCESS;
+      case 'warning':
+      default:
+        return ALERT_TYPE.WARNING;
+    }
+  };
+
+  Toast.show({
+    type: getType(),
+    title: title,
+    textBody: textBody,
+  });
+};
+
+export default toastShow;
