2fa7bdeb95d12b4ab77c08d8ef0d45879e81ce24
Isaac Cruz <isaac.cruz@kartado.com.br>
2023-06-05 20:02:29 -0300

    Change the provideer context to get a searchParams object

--- src/context/appContext.tsx
+++ src/context/appContext.tsx
@@ -3,21 +3,25 @@ import {createContext} from 'react';
 interface globalStateType {
   keyboardIsVisible: boolean;
   showDialogEditNote: boolean;
+  searchParams: object;
 }
 
 interface ContextType {
   globalState: globalStateType;
   setKeyboardIsVisible: React.Dispatch<React.SetStateAction<boolean>>;
   setShowDialogEditNote: React.Dispatch<React.SetStateAction<boolean>>;
+  setSearchParams: React.Dispatch<React.SetStateAction<object>>;
 }
 
 const AppContext = createContext<ContextType>({
   globalState: {
     keyboardIsVisible: false,
     showDialogEditNote: false,
+    searchParams: {},
   },
   setKeyboardIsVisible: () => {},
   setShowDialogEditNote: () => {},
+  setSearchParams: () => {},
 });
 
 export default AppContext;
--- src/context/appProvider.tsx
+++ src/context/appProvider.tsx
@@ -1,23 +1,26 @@
-import React, {useState} from 'react';
+import React, {ReactNode, useState} from 'react';
 import AppContext from './appContext';
 
 export interface ProviderProps {
-  children: JSX.Element | any;
+  children: ReactNode;
 }
 
 const ContextProvider = ({children}: ProviderProps | any) => {
   const [keyboardIsVisible, setKeyboardIsVisible] = useState(false);
   const [showDialogEditNote, setShowDialogEditNote] = useState(false);
+  const [searchParams, setSearchParams] = useState({});
 
   const globalState = {
     keyboardIsVisible,
     showDialogEditNote,
+    searchParams,
   };
 
   const contextValue = {
     globalState,
     setKeyboardIsVisible,
     setShowDialogEditNote,
+    setSearchParams,
   };
 
   return (
