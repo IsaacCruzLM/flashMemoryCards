602e83f4acacd8677f870f27271bcea41bc59c4b
Isaac Cruz <isaac.cruz@kartado.com.br>
2023-06-30 16:28:55 -0300

    FIX rangeInputDateVerify

--- src/utils/filters/dates.ts
+++ src/utils/filters/dates.ts
@@ -1,11 +1,28 @@
 import {rangeDataType} from '../../components/DataRangeInput/types';
 
-const rangeInputDateVerify = (data: rangeDataType) => {
-  const initDate = new Date(data.init as Date).getTime();
-  const endDate = new Date(data.end as Date).getTime();
-  const today = new Date().getTime();
+const DEFAULT_INIT_DATE = '1970-01-01T00:00:00.000Z';
+const DEFAULT_END_DATE = '2999-01-01T00:00:00.000Z';
 
-  return initDate <= today && endDate >= today;
+const rangeInputDateVerify = (data: rangeDataType, date: string) => {
+  const initDate = new Date(
+    new Date((data.init ? data.init : DEFAULT_INIT_DATE) as Date).setHours(
+      0,
+      0,
+      0,
+      0,
+    ),
+  ).getTime();
+  const endDate = new Date(
+    new Date((data.end ? data.end : DEFAULT_END_DATE) as Date).setHours(
+      0,
+      0,
+      0,
+      0,
+    ),
+  ).getTime();
+  const verificatedDate = new Date(date).getTime();
+
+  return initDate <= verificatedDate && endDate >= verificatedDate;
 };
 
 export default rangeInputDateVerify;
