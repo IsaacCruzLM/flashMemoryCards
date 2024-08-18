2a8c115c075b8ed70e4b29d3c19c224ddebca9c9
IsaacCruzLM <isaac.clm1@gmail.com>
2024-08-18 12:05:58 -0300

    feat: Add onNotification behavior

--- index.js
+++ index.js
@@ -8,17 +8,21 @@ import App from './App';
 import {name as appName} from './app.json';
 import PushNotification from 'react-native-push-notification';
 
+import NavigationService from './src/navigation/NavigationService';
+
 PushNotification.configure({
   // (optional) Called when Token is generated (iOS and Android)
   onRegister: function (token) {
     console.log('TOKEN:', token);
   },
 
-  // (required) Called when a remote is received or opened, or local notification is opened
   onNotification: function (notification) {
-    console.log('NOTIFICATION:', notification);
-
-    // process the notification
+    if (notification.data.title && notification.data.id) {
+      NavigationService.navigate('ShowNote', {
+        noteName: notification.data.title,
+        noteId: notification.data.id,
+      });
+    }
   },
 
   // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
@@ -33,25 +37,12 @@ PushNotification.configure({
   onRegistrationError: function (err) {
     console.error(err.message, err);
   },
-
-  // IOS ONLY (optional): default: all - Permissions to register.
   permissions: {
     alert: true,
     badge: true,
     sound: true,
   },
-
-  // Should the initial notification be popped automatically
-  // default: true
   popInitialNotification: true,
-
-  /**
-   * (optional) default: true
-   * - Specified if permissions (ios) and token (android and ios) will requested or not,
-   * - if not, you must call PushNotificationsHandler.requestPermissions() later
-   * - if you are not using remote notification or do not have Firebase installed, use this:
-   *     requestPermissions: Platform.OS === 'ios'
-   */
   requestPermissions: true,
 });
 
