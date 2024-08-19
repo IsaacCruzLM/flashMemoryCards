161be562a2310a597accc61d740a8c45e1248ee6
IsaacCruzLM <isaac.clm1@gmail.com>
2024-08-19 20:23:13 -0300

    feat: add title in schedule local noification

--- src/utils/notifications/scheduleNotification.ts
+++ src/utils/notifications/scheduleNotification.ts
@@ -2,12 +2,14 @@ import PushNotification from 'react-native-push-notification';
 
 const scheduleLocalNotification = (
   channelId: string,
+  title: string,
   message: string,
   date: Date,
   userInfo: Object = {},
 ): void =>
   PushNotification.localNotificationSchedule({
     channelId: channelId,
+    title: title,
     message: message,
     userInfo: userInfo,
     date: date,
