3a992b20299ad7230d488fafb5bb3b76ca543827
IsaacCruzLM <isaac.clm1@gmail.com>
2025-01-13 20:44:54 -0300

    feat: add tests to subjectList

--- src/screens/Subject/List/subjectList.test.tsx
+++ src/screens/Subject/List/subjectList.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import renderer, {ReactTestInstance} from 'react-test-renderer';
 import List from './index';
 
-describe('Note List Screen', () => {
+describe('Subject List Screen', () => {
   let tree: renderer.ReactTestRenderer;
   beforeEach(() => {
     jest.clearAllMocks();
@@ -23,6 +23,21 @@ describe('Note List Screen', () => {
   it('should render flat list', () => {
     const flatList = tree.root.findByType('RCTScrollView' as React.ElementType);
     expect(flatList).toBeTruthy();
+  });
+
+  it('should render should render two subjects', () => {
+    const testInstance = tree.root;
+
+    const matchingTexts = testInstance.findAll((node: ReactTestInstance) => {
+      return (
+        (node.type as string) === 'Text' &&
+        (node.children as string[]).some((text: string) =>
+          text.includes('Assunto'),
+        )
+      );
+    });
+
+    expect(matchingTexts.length).toBe(2);
   });
 
   it('should render floating button', () => {
